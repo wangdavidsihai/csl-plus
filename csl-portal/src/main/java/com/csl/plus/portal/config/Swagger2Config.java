6dbc052faa93755e226b82c6f5e0eb45bbecfd7d
Sihai.wang@sc.com <Sihai.wang@sc.com>
2019-12-22 16:27:36 +0800

    change for swagger

--- csl-portal/src/main/java/com/csl/plus/portal/config/Swagger2Config.java
+++ csl-portal/src/main/java/com/csl/plus/portal/config/Swagger2Config.java
@@ -3,9 +3,13 @@ package com.csl.plus.portal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,16 +21,44 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+	// 定义分隔符
+	private static final String splitor = ";";
+
 	@Bean
 	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("com.csl.plus.portal.single")).paths(PathSelectors.any())
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2)
+				// 详细定制
+				.apiInfo(apiInfo()).select()
+				// 指定当前包路径，这里就添加了两个包，注意方法变成了basePackage，中间加上成员变量splitor
+				.apis(basePackage("com.csl.plus.portal.single"))
+				// 扫描所有 .apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("csl-plus 前台API")
 				.description("csl-plus 前台展现层API列表，只支持**/list 类型接口无状态调用，其他接口需要安全验证").contact("csl-plus").version("1.0")
 				.build();
+	}
+
+	public static Predicate<RequestHandler> basePackage(final String basePackage) {
+		return input -> declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+	}
+
+	private static Function<Class<?>, Boolean> handlerPackage(final String basePackage) {
+		return input -> {
+			// 循环判断匹配
+			for (String strPackage : basePackage.split(splitor)) {
+				boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+				if (isMatch) {
+					return true;
+				}
+			}
+			return false;
+		};
+	}
+
+	private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+		return Optional.fromNullable(input.declaringClass());
 	}
 }
