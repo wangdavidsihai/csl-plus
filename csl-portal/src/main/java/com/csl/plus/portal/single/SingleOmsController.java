ad4c454033b91731e484a4c66e2a15d81cab0440
Sihai.wang@sc.com <Sihai.wang@sc.com>
2020-02-13 22:05:00 +0800

    ignore some apis

--- csl-portal/src/main/java/com/csl/plus/portal/single/SingleOmsController.java
+++ csl-portal/src/main/java/com/csl/plus/portal/single/SingleOmsController.java
@@ -1,13 +1,5 @@
 package com.csl.plus.portal.single;
 
-import javax.annotation.Resource;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.csl.plus.oms.entity.OmsOrder;
@@ -18,62 +10,66 @@ import com.csl.plus.portal.marking.service.ISmsGroupService;
 import com.csl.plus.portal.oms.service.IOmsOrderService;
 import com.csl.plus.ums.entity.UmsMember;
 import com.csl.plus.utils.CommonResult;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.annotation.Resource;
 
 /**
  * @Date: 2019/4/2 15:02
  * @Description:
  */
+@ApiIgnore
 @RestController
 @Api(tags = "SingleOmsController", description = "订单管理系统")
 @RequestMapping("/api/single/oms")
 public class SingleOmsController extends ApiBaseAction {
 
-	@Resource
-	private ISmsGroupService groupService;
-	@Resource
-	private IOmsOrderService orderService;
+    @Resource
+    private ISmsGroupService groupService;
+    @Resource
+    private IOmsOrderService orderService;
 
-	@IgnoreAuth
-	@SysLog(MODULE = "oms", REMARK = "查询订单列表")
-	@ApiOperation(value = "查询订单列表")
-	@GetMapping(value = "/order/list")
-	public Object orderList(OmsOrder order,
-			@RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
-			@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-		return new CommonResult()
-				.success(orderService.page(new Page<OmsOrder>(pageNum, pageSize), new QueryWrapper<>(order)));
-	}
+    @IgnoreAuth
+    @SysLog(MODULE = "oms", REMARK = "查询订单列表")
+    @ApiOperation(value = "查询订单列表")
+    @GetMapping(value = "/order/list")
+    public Object orderList(OmsOrder order,
+                            @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
+                            @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
+        return new CommonResult()
+                .success(orderService.page(new Page<OmsOrder>(pageNum, pageSize), new QueryWrapper<>(order)));
+    }
 
-	/**
-	 * 提交订单
-	 * 
-	 * @param orderParam
-	 * @return
-	 */
-	@ApiOperation("商品详情预览订单")
-	@SysLog(MODULE = "order", REMARK = "商品详情预览订单")
-	@GetMapping(value = "/preOrder")
-	public Object preOrder(GroupAndOrderVo orderParam) {
-		UmsMember member = this.getCurrentMember();
-		orderParam.setMemberId(member.getId());
-		orderParam.setName(member.getNickname());
-		return orderService.preSingelOrder(orderParam);
-	}
+    /**
+     * 提交订单
+     *
+     * @param orderParam
+     * @return
+     */
+    @ApiOperation("商品详情预览订单")
+    @SysLog(MODULE = "order", REMARK = "商品详情预览订单")
+    @GetMapping(value = "/preOrder")
+    public Object preOrder(GroupAndOrderVo orderParam) {
+        UmsMember member = this.getCurrentMember();
+        orderParam.setMemberId(member.getId());
+        orderParam.setName(member.getNickname());
+        return orderService.preSingelOrder(orderParam);
+    }
 
-	/**
-	 * 提交订单
-	 * 
-	 * @param orderParam
-	 * @return
-	 */
-	@ApiOperation("商品详情生成订单")
-	@SysLog(MODULE = "order", REMARK = "商品详情生成订单")
-	@PostMapping(value = "/bookOrder")
-	public Object bookOrder(GroupAndOrderVo orderParam) {
-		UmsMember member = this.getCurrentMember();
-		return orderService.generateSingleOrder(orderParam, member);
-	}
+    /**
+     * 提交订单
+     *
+     * @param orderParam
+     * @return
+     */
+    @ApiOperation("商品详情生成订单")
+    @SysLog(MODULE = "order", REMARK = "商品详情生成订单")
+    @PostMapping(value = "/bookOrder")
+    public Object bookOrder(GroupAndOrderVo orderParam) {
+        UmsMember member = this.getCurrentMember();
+        return orderService.generateSingleOrder(orderParam, member);
+    }
 }
--- csl-portal/src/main/java/com/csl/plus/portal/single/SinglePmsController.java
+++ csl-portal/src/main/java/com/csl/plus/portal/single/SinglePmsController.java
@@ -1,18 +1,5 @@
 package com.csl.plus.portal.single;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.csl.plus.marking.entity.SmsGroup;
@@ -33,113 +20,121 @@ import com.csl.plus.portal.util.DateUtils;
 import com.csl.plus.ums.entity.UmsMember;
 import com.csl.plus.ums.entity.UmsMemberLevel;
 import com.csl.plus.utils.CommonResult;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Date: 2019/4/2 15:02
  * @Description:
  */
+@ApiIgnore
 @RestController
 @Api(tags = "CmsController", description = "商品关系管理")
 @RequestMapping("/api/single/pms")
 public class SinglePmsController extends ApiBaseAction {
 
-	@Resource
-	private ISmsGroupService groupService;
-	@Resource
-	private IUmsMemberLevelService memberLevelService;
-	@Resource
-	private IPmsProductService pmsProductService;
-	@Resource
-	private IPmsProductAttributeCategoryService productAttributeCategoryService;
-	@Resource
-	private IPmsProductCategoryService productCategoryService;
+    @Resource
+    private ISmsGroupService groupService;
+    @Resource
+    private IUmsMemberLevelService memberLevelService;
+    @Resource
+    private IPmsProductService pmsProductService;
+    @Resource
+    private IPmsProductAttributeCategoryService productAttributeCategoryService;
+    @Resource
+    private IPmsProductCategoryService productCategoryService;
 
-	@Resource
-	private ICmsSubjectCategoryService subjectCategoryService;
-	@Resource
-	private ICmsSubjectService subjectService;
-	@Resource
-	private ICmsSubjectCommentService commentService;
+    @Resource
+    private ICmsSubjectCategoryService subjectCategoryService;
+    @Resource
+    private ICmsSubjectService subjectService;
+    @Resource
+    private ICmsSubjectCommentService commentService;
 
-	@SysLog(MODULE = "pms", REMARK = "查询商品详情信息")
-	@IgnoreAuth
-	@GetMapping(value = "/goods/detail")
-	@ApiOperation(value = "查询商品详情信息")
-	public Object queryProductDetail(@RequestParam(value = "id", required = false, defaultValue = "0") Long id) {
-		PmsProductAndGroup productResult = pmsProductService.getProductAndGroup(id);
-		Map<String, Object> map = new HashMap<>();
-		SmsGroup queryGoods = new SmsGroup();
-		queryGoods.setGoodsId(id);
-		SmsGroup group = groupService.getOne(new QueryWrapper<>(queryGoods));
-		if (group != null) {
-			Date endTime = DateUtils.convertStringToDate(DateUtils.addHours(group.getEndTime(), group.getHours()),
-					"yyyy-MM-dd HH:mm:ss");
-			Long nowT = System.currentTimeMillis();
-			if (group != null && nowT > group.getStartTime().getTime() && nowT < endTime.getTime()) {
-				map.put("group", group);
-				map.put("isGroup", 1);
-			} else {
-				map.put("isGroup", 2);
-			}
-		}
+    @SysLog(MODULE = "pms", REMARK = "查询商品详情信息")
+    @IgnoreAuth
+    @GetMapping(value = "/goods/detail")
+    @ApiOperation(value = "查询商品详情信息")
+    public Object queryProductDetail(@RequestParam(value = "id", required = false, defaultValue = "0") Long id) {
+        PmsProductAndGroup productResult = pmsProductService.getProductAndGroup(id);
+        Map<String, Object> map = new HashMap<>();
+        SmsGroup queryGoods = new SmsGroup();
+        queryGoods.setGoodsId(id);
+        SmsGroup group = groupService.getOne(new QueryWrapper<>(queryGoods));
+        if (group != null) {
+            Date endTime = DateUtils.convertStringToDate(DateUtils.addHours(group.getEndTime(), group.getHours()),
+                    "yyyy-MM-dd HH:mm:ss");
+            Long nowT = System.currentTimeMillis();
+            if (group != null && nowT > group.getStartTime().getTime() && nowT < endTime.getTime()) {
+                map.put("group", group);
+                map.put("isGroup", 1);
+            } else {
+                map.put("isGroup", 2);
+            }
+        }
 
-		map.put("goods", productResult);
-		return new CommonResult().success(map);
-	}
+        map.put("goods", productResult);
+        return new CommonResult().success(map);
+    }
 
-	@SysLog(MODULE = "pms", REMARK = "查询商品列表")
-	@IgnoreAuth
-	@ApiOperation(value = "查询商品列表")
-	@GetMapping(value = "/goods/list")
-	public Object goodsList(PmsProduct product,
-			@RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
-			@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-		product.setPublishStatus(1);
-		product.setVerifyStatus(1);
-		return new CommonResult()
-				.success(pmsProductService.page(new Page<PmsProduct>(pageNum, pageSize), new QueryWrapper<>(product)));
-	}
+    @SysLog(MODULE = "pms", REMARK = "查询商品列表")
+    @IgnoreAuth
+    @ApiOperation(value = "查询商品列表")
+    @GetMapping(value = "/goods/list")
+    public Object goodsList(PmsProduct product,
+                            @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
+                            @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
+        product.setPublishStatus(1);
+        product.setVerifyStatus(1);
+        return new CommonResult()
+                .success(pmsProductService.page(new Page<PmsProduct>(pageNum, pageSize), new QueryWrapper<>(product)));
+    }
 
-	@SysLog(MODULE = "pms", REMARK = "查询商品分类列表")
-	@IgnoreAuth
-	@ApiOperation(value = "查询商品分类列表")
-	@GetMapping(value = "/productCategory/list")
-	public Object productCategoryList(PmsProductCategory productCategory,
-			@RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
-			@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-		return new CommonResult().success(productCategoryService.page(new Page<PmsProductCategory>(pageNum, pageSize),
-				new QueryWrapper<>(productCategory)));
-	}
+    @SysLog(MODULE = "pms", REMARK = "查询商品分类列表")
+    @IgnoreAuth
+    @ApiOperation(value = "查询商品分类列表")
+    @GetMapping(value = "/productCategory/list")
+    public Object productCategoryList(PmsProductCategory productCategory,
+                                      @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize,
+                                      @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
+        return new CommonResult().success(productCategoryService.page(new Page<PmsProductCategory>(pageNum, pageSize),
+                new QueryWrapper<>(productCategory)));
+    }
 
-	@ApiOperation("创建商品")
-	@SysLog(MODULE = "pms", REMARK = "创建商品")
-	@PostMapping(value = "/createGoods")
-	public Object createGoods(PmsProduct productParam) {
-		CommonResult commonResult;
-		UmsMember member = this.getCurrentMember();
-		if (member.getMemberLevelId() > 0) {
-			UmsMemberLevel memberLevel = memberLevelService.getById(member.getMemberLevelId());
-			PmsProduct newSubject = new PmsProduct();
-			newSubject.setSupplyId(member.getId());
-			newSubject.setPublishStatus(1);
-			newSubject.setVerifyStatus(1);
-			List<PmsProduct> subjects = pmsProductService.list(new QueryWrapper<>(newSubject));
-			if (subjects != null && subjects.size() > memberLevel.getGoodscount()) {
-				commonResult = new CommonResult().failed("你今天已经有发" + memberLevel.getGoodscount() + "个商品");
-				return commonResult;
-			}
-		}
-		productParam.setSupplyId(member.getId());
-		productParam.setCreateTime(new Date());
-		boolean count = pmsProductService.save(productParam);
-		if (count) {
-			return new CommonResult().success(count);
-		} else {
-			return new CommonResult().failed();
-		}
-	}
+    @ApiOperation("创建商品")
+    @SysLog(MODULE = "pms", REMARK = "创建商品")
+    @PostMapping(value = "/createGoods")
+    public Object createGoods(PmsProduct productParam) {
+        CommonResult commonResult;
+        UmsMember member = this.getCurrentMember();
+        if (member.getMemberLevelId() > 0) {
+            UmsMemberLevel memberLevel = memberLevelService.getById(member.getMemberLevelId());
+            PmsProduct newSubject = new PmsProduct();
+            newSubject.setSupplyId(member.getId());
+            newSubject.setPublishStatus(1);
+            newSubject.setVerifyStatus(1);
+            List<PmsProduct> subjects = pmsProductService.list(new QueryWrapper<>(newSubject));
+            if (subjects != null && subjects.size() > memberLevel.getGoodscount()) {
+                commonResult = new CommonResult().failed("你今天已经有发" + memberLevel.getGoodscount() + "个商品");
+                return commonResult;
+            }
+        }
+        productParam.setSupplyId(member.getId());
+        productParam.setCreateTime(new Date());
+        boolean count = pmsProductService.save(productParam);
+        if (count) {
+            return new CommonResult().success(count);
+        } else {
+            return new CommonResult().failed();
+        }
+    }
 
 }
