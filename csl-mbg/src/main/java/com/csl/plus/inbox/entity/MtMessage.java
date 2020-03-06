91dc7ebd645ea3986b431bfbf02e4d8c3c3e5eba
Sihai.wang@sc.com <Sihai.wang@sc.com>
2020-03-06 22:52:54 +0800

    fix issue of mt message

--- csl-mbg/src/main/java/com/csl/plus/inbox/entity/MtMessage.java
+++ csl-mbg/src/main/java/com/csl/plus/inbox/entity/MtMessage.java
@@ -3,56 +3,59 @@ package com.csl.plus.inbox.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * 消息通知表
- * 
+ *
  * @author David
- * @email 
+ * @email
  * @date 2020-01-31 17:46:11
  */
 @Data
 @TableName("mt_message")
 public class MtMessage implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 主键
-	 */
-	@TableId(type = IdType.UUID)
-	private String id;
-	/**
-	 * 发送者ID
-	 */
-	private String sendid;
-	/**
-	 * 接收者ID
-	 */
-	private String recid;
-	/**
-	 * 消息ID
-	 */
-	private String msgid;
-	/**
-	 * 创建时间
-	 */
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date createDate;
-	/**
-	 * 状态
-	 */
-	private String status;
-	/**
-	 * 删除标识
-	 */
-	private String delFlag;
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.UUID)
+    private String id;
+    /**
+     * 标题
+     */
+    private String subject;
+    /**
+     * 发送者ID
+     */
+    private String sendid;
+    /**
+     * 接收者ID
+     */
+    private String recid;
+    /**
+     * 消息ID
+     */
+    private String msgid;
+    /**
+     * 创建时间
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createDate;
+    /**
+     * 状态
+     */
+    private String status;
+    /**
+     * 删除标识
+     */
+    private String delFlag;
 
 }
