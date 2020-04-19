8ba61162baae2acbf9efceffe7f2a0fe8addf6a8
Sihai.wang@sc.com <Sihai.wang@sc.com>
2020-04-19 23:04:04 +0800

    change for invitation code

--- csl-admin/src/main/java/com/csl/plus/marking/controller/SmsCouponController.java
+++ csl-admin/src/main/java/com/csl/plus/marking/controller/SmsCouponController.java
@@ -8,7 +8,6 @@ import com.csl.plus.marking.service.ISmsCouponService;
 import com.csl.plus.marking.vo.SmsCouponParam;
 import com.csl.plus.utils.CommonResult;
 import com.csl.plus.utils.ValidatorUtils;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -20,21 +19,21 @@ import javax.annotation.Resource;
 
 /**
  * <p>
- * 优惠卷表
+ * 邀请码表
  * </p>
  *
  * @since 2019-04-19
  */
 @Slf4j
 @RestController
-@Api(tags = "SmsCouponController", description = "优惠卷表管理")
+@Api(tags = "SmsCouponController", description = "邀请码表管理")
 @RequestMapping("/marking/SmsCoupon")
 public class SmsCouponController {
     @Resource
     private ISmsCouponService ISmsCouponService;
 
-    @SysLog(MODULE = "marking", REMARK = "根据条件查询所有优惠卷表列表")
-    @ApiOperation("根据条件查询所有优惠卷表列表")
+    @SysLog(MODULE = "marking", REMARK = "根据条件查询所有邀请码表列表")
+    @ApiOperation("根据条件查询所有邀请码表列表")
     @GetMapping(value = "/list")
     @PreAuthorize("hasAuthority('marking:SmsCoupon:read')")
     public Object getSmsCouponByPage(SmsCoupon entity,
@@ -44,13 +43,13 @@ public class SmsCouponController {
         try {
             return new CommonResult().success(ISmsCouponService.page(new Page<SmsCoupon>(pageNum, pageSize), new QueryWrapper<>(entity)));
         } catch (Exception e) {
-            log.error("根据条件查询所有优惠卷表列表：%s", e.getMessage(), e);
+            log.error("根据条件查询所有邀请码表列表：%s", e.getMessage(), e);
         }
         return new CommonResult().failed();
     }
 
-    @SysLog(MODULE = "marking", REMARK = "保存优惠卷表")
-    @ApiOperation("保存优惠卷表")
+    @SysLog(MODULE = "marking", REMARK = "保存邀请码表")
+    @ApiOperation("保存邀请码表")
     @PostMapping(value = "/create")
     @PreAuthorize("hasAuthority('marking:SmsCoupon:create')")
     public Object saveSmsCoupon(@RequestBody SmsCouponParam entity) {
@@ -59,14 +58,14 @@ public class SmsCouponController {
                 return new CommonResult().success();
             }
         } catch (Exception e) {
-            log.error("保存优惠卷表：%s", e.getMessage(), e);
+            log.error("保存邀请码表：%s", e.getMessage(), e);
             return new CommonResult().failed();
         }
         return new CommonResult().failed();
     }
 
-    @SysLog(MODULE = "marking", REMARK = "更新优惠卷表")
-    @ApiOperation("更新优惠卷表")
+    @SysLog(MODULE = "marking", REMARK = "更新邀请码表")
+    @ApiOperation("更新邀请码表")
     @PostMapping(value = "/update/{id}")
     @PreAuthorize("hasAuthority('marking:SmsCoupon:update')")
     public Object updateSmsCoupon(@RequestBody SmsCouponParam entity) {
@@ -75,49 +74,48 @@ public class SmsCouponController {
                 return new CommonResult().success();
             }
         } catch (Exception e) {
-            log.error("更新优惠卷表：%s", e.getMessage(), e);
+            log.error("更新邀请码表：%s", e.getMessage(), e);
             return new CommonResult().failed();
         }
         return new CommonResult().failed();
     }
 
-    @SysLog(MODULE = "marking", REMARK = "删除优惠卷表")
-    @ApiOperation("删除优惠卷表")
+    @SysLog(MODULE = "marking", REMARK = "删除邀请码表")
+    @ApiOperation("删除邀请码表")
     @DeleteMapping(value = "/delete/{id}")
     @PreAuthorize("hasAuthority('marking:SmsCoupon:delete')")
-    public Object deleteSmsCoupon(@ApiParam("优惠卷表id") @PathVariable Long id) {
+    public Object deleteSmsCoupon(@ApiParam("邀请码表id") @PathVariable Long id) {
         try {
             if (ValidatorUtils.empty(id)) {
-                return new CommonResult().paramFailed("优惠卷表id");
+                return new CommonResult().paramFailed("邀请码表id");
             }
-            if (ISmsCouponService.delete(id)>0) {
+            if (ISmsCouponService.delete(id) > 0) {
                 return new CommonResult().success();
             }
         } catch (Exception e) {
-            log.error("删除优惠卷表：%s", e.getMessage(), e);
+            log.error("删除邀请码表：%s", e.getMessage(), e);
             return new CommonResult().failed();
         }
         return new CommonResult().failed();
     }
 
-    @SysLog(MODULE = "marking", REMARK = "给优惠卷表分配优惠卷表")
-    @ApiOperation("查询优惠卷表明细")
+    @SysLog(MODULE = "marking", REMARK = "给邀请码表分配邀请码表")
+    @ApiOperation("查询邀请码表明细")
     @GetMapping(value = "/{id}")
     @PreAuthorize("hasAuthority('marking:SmsCoupon:read')")
-    public Object getSmsCouponById(@ApiParam("优惠卷表id") @PathVariable Long id) {
+    public Object getSmsCouponById(@ApiParam("邀请码表id") @PathVariable Long id) {
         try {
             if (ValidatorUtils.empty(id)) {
-                return new CommonResult().paramFailed("优惠卷表id");
+                return new CommonResult().paramFailed("邀请码表id");
             }
             SmsCouponParam coupon = ISmsCouponService.getItem(id);
             return new CommonResult().success(coupon);
         } catch (Exception e) {
-            log.error("查询优惠卷表明细：%s", e.getMessage(), e);
+            log.error("查询邀请码表明细：%s", e.getMessage(), e);
             return new CommonResult().failed();
         }
 
     }
-
 
 
 }
--- csl-admin/src/main/java/com/csl/plus/marking/service/impl/SmsCouponServiceImpl.java
+++ csl-admin/src/main/java/com/csl/plus/marking/service/impl/SmsCouponServiceImpl.java
@@ -12,15 +12,13 @@ import com.csl.plus.marking.service.ISmsCouponProductCategoryRelationService;
 import com.csl.plus.marking.service.ISmsCouponProductRelationService;
 import com.csl.plus.marking.service.ISmsCouponService;
 import com.csl.plus.marking.vo.SmsCouponParam;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 /**
  * <p>
- * 优惠卷表 服务实现类
+ * 邀请码表 服务实现类
  * </p>
  *
  * @since 2019-04-19
@@ -44,22 +42,22 @@ public class SmsCouponServiceImpl extends ServiceImpl<SmsCouponMapper, SmsCoupon
         couponParam.setCount(couponParam.getPublishCount());
         couponParam.setUseCount(0);
         couponParam.setReceiveCount(0);
-        //插入优惠券表
+        //插入邀请码表
         int count = couponMapper.insert(couponParam);
-        //插入优惠券和商品关系表
+        //插入邀请码和商品关系表
         if (couponParam.getUseType().equals(2)) {
             for (SmsCouponProductRelation productRelation : couponParam.getProductRelationList()) {
                 productRelation.setCouponId(couponParam.getId());
             }
             productRelationDao.saveBatch(couponParam.getProductRelationList());
         }
-        //插入优惠券和商品分类关系表
-        if (couponParam.getUseType().equals(1)) {
-            for (SmsCouponProductCategoryRelation couponProductCategoryRelation : couponParam.getProductCategoryRelationList()) {
-                couponProductCategoryRelation.setCouponId(couponParam.getId());
-            }
-            productCategoryRelationDao.saveBatch(couponParam.getProductCategoryRelationList());
-        }
+        //插入邀请码和商品分类关系表
+//        if (couponParam.getUseType().equals(1)) {
+//            for (SmsCouponProductCategoryRelation couponProductCategoryRelation : couponParam.getProductCategoryRelationList()) {
+//                couponProductCategoryRelation.setCouponId(couponParam.getId());
+//            }
+//            productCategoryRelationDao.saveBatch(couponParam.getProductCategoryRelationList());
+//        }
         return true;
     }
 
@@ -67,7 +65,7 @@ public class SmsCouponServiceImpl extends ServiceImpl<SmsCouponMapper, SmsCoupon
     public boolean updateByIds(SmsCouponParam couponParam) {
         couponParam.setId(couponParam.getId());
         int count = couponMapper.updateById(couponParam);
-        //删除后插入优惠券和商品关系表
+        //删除后插入邀请码和商品关系表
         if (couponParam.getUseType().equals(2)) {
             for (SmsCouponProductRelation productRelation : couponParam.getProductRelationList()) {
                 productRelation.setCouponId(couponParam.getId());
@@ -75,7 +73,7 @@ public class SmsCouponServiceImpl extends ServiceImpl<SmsCouponMapper, SmsCoupon
             deleteProductRelation(couponParam.getId());
             productRelationDao.saveBatch(couponParam.getProductRelationList());
         }
-        //删除后插入优惠券和商品分类关系表
+        //删除后插入邀请码和商品分类关系表
         if (couponParam.getUseType().equals(1)) {
             for (SmsCouponProductCategoryRelation couponProductCategoryRelation : couponParam.getProductCategoryRelationList()) {
                 couponProductCategoryRelation.setCouponId(couponParam.getId());
@@ -85,17 +83,18 @@ public class SmsCouponServiceImpl extends ServiceImpl<SmsCouponMapper, SmsCoupon
         }
         return true;
     }
+
     private void deleteProductCategoryRelation(Long id) {
-        productCategoryRelationMapper.delete(new QueryWrapper<>(new SmsCouponProductCategoryRelation()).eq("coupon_id",id));
+        productCategoryRelationMapper.delete(new QueryWrapper<>(new SmsCouponProductCategoryRelation()).eq("coupon_id", id));
     }
 
     private void deleteProductRelation(Long id) {
-        productRelationMapper.delete(new QueryWrapper<>(new SmsCouponProductRelation()).eq("coupon_id",id));
+        productRelationMapper.delete(new QueryWrapper<>(new SmsCouponProductRelation()).eq("coupon_id", id));
     }
 
     @Override
     public int delete(Long id) {
-        //删除优惠券
+        //删除邀请码
         int count = couponMapper.deleteById(id);
         //删除商品关联
         deleteProductRelation(id);
